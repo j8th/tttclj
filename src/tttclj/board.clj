ab823de1d4b7587e3292150017b0d2c7e71bf95c
Jason <jason@Jasons-MacBook-Pro.local>
2015-01-28 15:25:04 -0600

    Extract methods x-spots, o-spots, and open-spots from the rules namespace and put them into the board namespace.

--- src/tttclj/board.clj
+++ src/tttclj/board.clj
@@ -1,10 +1,29 @@
-(ns tttclj.board)
+(ns tttclj.board
+  (:require [tttclj.player.token :refer [token-x token-o]]))
 
-(def empty-spot ".")
-(def board-size 9)
-(def new-board [empty-spot empty-spot empty-spot
-                empty-spot empty-spot empty-spot
-                empty-spot empty-spot empty-spot])
+(let [
+      get-token-indices (fn [board, token] 
+        (map first 
+          (filter #(= (second %) token)
+            (map-indexed vector board))))
+     ]
 
-(defn drop-piece [board spot token]
-  (assoc board spot token))
+  (def empty-spot ".")
+  (def board-size 9)
+  (def new-board [empty-spot empty-spot empty-spot
+                  empty-spot empty-spot empty-spot
+                  empty-spot empty-spot empty-spot])
+
+  (defn drop-piece [board spot token]
+    (assoc board spot token))
+
+  (defn x-spots [board] 
+    (set (get-token-indices board token-x)))
+
+  (defn o-spots [board] 
+    (set (get-token-indices board token-o)))
+
+  (defn open-spots [board]
+    (set (get-token-indices board empty-spot)))
+
+)
--- src/tttclj/rules.clj
+++ src/tttclj/rules.clj
@@ -1,9 +1,10 @@
 (ns tttclj.rules
   (:require [tttclj.player.token :refer [token-x token-o]]
-            [tttclj.board :refer [empty-spot]]
+            [tttclj.board :refer [empty-spot x-spots o-spots open-spots]]
             [clojure.set :refer [subset?]]))
 
-(let [winning-combinations [#{0 1 2}
+(let [
+     winning-combinations [#{0 1 2}
                             #{3 4 5}
                             #{6 7 8}
                             #{0 3 6}
@@ -11,20 +12,6 @@
                             #{2 5 8}
                             #{0 4 8}
                             #{6 4 2}]
-
-      get-token-indices (fn [board, token] 
-                     (map first 
-                       (filter #(= (second %) token)
-                         (map-indexed vector board))))
-
-      x-spots (fn [board] 
-                    (set (get-token-indices board token-x)))
-
-      o-spots (fn [board] 
-                    (set (get-token-indices board token-o)))
-
-      open-spots (fn [board]
-                    (set (get-token-indices board empty-spot)))
 
       contains-winning-combination? (fn [set-of-tokens]
                                       (not (empty? (filter #(subset? % set-of-tokens) winning-combinations))))
