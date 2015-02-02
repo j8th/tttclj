ecb741b5862edca7b56bdaf1e71aa1447d51b019
Jason <jason@Jasons-MacBook-Pro.local>
2015-02-02 11:33:17 -0600

    Formatting fix.

--- src/tttclj/rules.clj
+++ src/tttclj/rules.clj
@@ -5,13 +5,13 @@
 
 (let [
      winning-combinations [#{0 1 2}
-                            #{3 4 5}
-                            #{6 7 8}
-                            #{0 3 6}
-                            #{1 4 7}
-                            #{2 5 8}
-                            #{0 4 8}
-                            #{6 4 2}]
+                           #{3 4 5}
+                           #{6 7 8}
+                           #{0 3 6}
+                           #{1 4 7}
+                           #{2 5 8}
+                           #{0 4 8}
+                           #{6 4 2}]
 
       contains-winning-combination? (fn [set-of-tokens]
                                       (not (empty? (filter #(subset? % set-of-tokens) winning-combinations))))
