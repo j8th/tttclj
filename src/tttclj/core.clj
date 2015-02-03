acba245f85c2b67886a7fb2a70ab2a04f665f758
Jason <jason@Jasons-MacBook-Pro.local>
2015-02-03 14:56:34 -0600

    Removed a few requires.

--- src/tttclj/core.clj
+++ src/tttclj/core.clj
@@ -1,11 +1,6 @@
 (ns tttclj.core
-  (:require [tttclj.board :refer [new-board]]
-            [tttclj.game :refer [play-game]]
-            [tttclj.player.human :as human]
-            [tttclj.player.ai :as ai]
-            [tttclj.printer :refer [print-winner]]
-            [tttclj.prompt :refer [prompt set-players]]
-            [tttclj.app :refer [app]]))
+  (:require [tttclj.app :refer [app]]
+            [tttclj.prompt :refer [prompt set-players]]))
 
 (defn -main [& args]
   (app (set-players) #(= (prompt "Play again? (y/n)" ["y" "n"]) "y"))
