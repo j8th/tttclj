a02f6e526cd4fceb7bfdefc9520ff3c09613d604
Jason <jason@Jasons-MacBook-Pro.local>
2015-02-03 10:12:00 -0600

    Implemented minimax.

--- src/tttclj/player/ai.clj
+++ src/tttclj/player/ai.clj
@@ -1,5 +1,20 @@
 (ns tttclj.player.ai
-  (:require [tttclj.board :refer [board-size open-spots]]))
+  (:require [tttclj.board :refer [board-size open-spots drop-piece]]
+            [tttclj.player.token :refer [token-x token-o]]
+            [tttclj.rules :refer [xturn? xwins? owins? draw? finished?]]
+            [tttclj.game :refer [play-game]]))
 
 (defn move [board]
-  (rand-nth (into [] (open-spots board))))
+  (let [open-spots (open-spots board)
+        token (if (xturn? board) token-x token-o)
+        score-board (fn [board]
+                     (let [final-board (play-game board move move)]
+                       (cond
+                         (xwins? final-board) 1
+                         (draw? final-board) 0
+                         (owins? final-board) -1)))
+        moves (zipmap open-spots (map #(score-board (drop-piece board % token)) open-spots))
+        maxormin (if (= token token-x) max-key min-key)
+        choice (key (apply maxormin val moves))]
+  choice))
+
